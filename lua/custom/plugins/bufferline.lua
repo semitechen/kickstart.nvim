ce01d65dba1a405402077e790c65ac70d675dceb
Kacper Jelonek <kapijelonek@gmail.com>
2026-03-28 19:01:06 +0100

    plugins I use

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,22 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+
+  lazy = false,
+
+  keys = {
+    { '<A-,>', '<Cmd>BufferLineCyclePrev<CR>', desc = 'Previous tab' },
+    { '<A-.>', '<Cmd>BufferLineCycleNext<CR>', desc = 'Next tab' },
+  },
+
+  config = function()
+    require('bufferline').setup {
+      options = {
+        always_show_bufferline = true,
+      },
+    }
+  end,
+}
