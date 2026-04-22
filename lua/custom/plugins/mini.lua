3315d21389b1eb8b127fe9a07615dd76c0006cd6
Kacper Jelonek <58530561+semitechen@users.noreply.github.com>
2026-04-22 17:48:30 +0200

    feat: mini statusline to match my tmux

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -11,15 +11,49 @@ return {
 
     -- Minimal, high-performance statusline
     local statusline = require 'mini.statusline'
-    statusline.setup { use_icons = vim.g.have_nerd_font }
+
+    local sep_left = ''
+    local sep_right = ''
+
+    local colors = {
+      blue = '#7aa2f7',
+      dark = '#15161e',
+      gray = '#3b4261',
+      bg = '#16161e',
+      fg = '#a9b1d6',
+      orange = '#e0af68',
+      purple = '#bb9af7',
+      red = '#f7768e',
+      cyan = '#7dcfff',
+    }
 
     -- Helper to merge theme colors for a seamless statusline
     local function sync_hl()
-      local function get_hl(name) return vim.api.nvim_get_hl(0, { name = name, link = false }) end
-      local dev_bg = get_hl('MiniStatuslineDevinfo').bg
-      local file_bg = get_hl('MiniStatuslineFilename').bg
+      vim.api.nvim_set_hl(0, 'StatusLine', { bg = colors.bg, fg = colors.fg })
 
-      -- Create seamless versions of diagnostic and git highlights
+      local modes = {
+        { name = 'Normal', color = colors.blue },
+        { name = 'Insert', color = colors.orange },
+        { name = 'Visual', color = colors.purple },
+        { name = 'Replace', color = colors.red },
+        { name = 'Command', color = colors.cyan },
+        { name = 'Other', color = colors.blue },
+      }
+
+      for _, m in ipairs(modes) do
+        local hl_name = 'MiniStatuslineMode' .. m.name
+        vim.api.nvim_set_hl(0, hl_name, { fg = colors.dark, bg = m.color, bold = true })
+        vim.api.nvim_set_hl(0, hl_name .. 'Ext', { fg = m.color, bg = colors.bg })
+        vim.api.nvim_set_hl(0, hl_name .. 'ToDevinfo', { fg = m.color, bg = colors.gray })
+        vim.api.nvim_set_hl(0, 'DevinfoTo' .. hl_name, { fg = m.color, bg = colors.gray })
+      end
+
+      vim.api.nvim_set_hl(0, 'MiniStatuslineDevinfo', { fg = colors.fg, bg = colors.gray })
+      vim.api.nvim_set_hl(0, 'MiniStatuslineDevinfoExt', { fg = colors.gray, bg = colors.bg })
+
+      vim.api.nvim_set_hl(0, 'MiniStatuslineFilename', { fg = colors.fg, bg = colors.bg })
+
+      local dev_bg = colors.gray
       local seamless_groups = {
         { name = 'StlError', fg = 'DiagnosticError', bg = dev_bg },
         { name = 'StlWarn', fg = 'DiagnosticWarn', bg = dev_bg },
@@ -27,14 +61,61 @@ return {
         { name = 'StlAdd', fg = 'GitSignsAdd', bg = dev_bg },
         { name = 'StlMod', fg = 'GitSignsChange', bg = dev_bg },
         { name = 'StlDel', fg = 'GitSignsDelete', bg = dev_bg },
-        { name = 'StlDot', fg = 'DiagnosticWarn', bg = file_bg },
       }
 
       for _, g in ipairs(seamless_groups) do
-        local fg = get_hl(g.fg).fg
-        vim.api.nvim_set_hl(0, g.name, { fg = fg, bg = g.bg })
+        local fg_color = vim.api.nvim_get_hl(0, { name = g.fg, link = false }).fg
+        vim.api.nvim_set_hl(0, g.name, { fg = fg_color, bg = g.bg })
       end
     end
+
+    statusline.setup {
+      use_icons = vim.g.have_nerd_font,
+      content = {
+        active = function()
+          local mode_str, mode_hl = statusline.section_mode { trunc_width = 120 }
+          local git = statusline.section_diff { trunc_width = 75 }
+          local diagnostics = statusline.section_diagnostics { trunc_width = 75 }
+          local filename = statusline.section_filename { trunc_width = 140 }
+          local fileinfo = statusline.section_fileinfo { trunc_width = 120 }
+          local location = statusline.section_location { trunc_width = 75 }
+          local search = statusline.section_searchcount { trunc_width = 75 }
+
+          local info_content = (git ~= '' and git or '') .. (diagnostics ~= '' and (' ' .. diagnostics) or '')
+          local left_segment = ''
+          if mode_str and mode_str ~= '' then
+            left_segment = string.format('%%#%sExt#%s%%#%s# %s ', mode_hl, sep_left, mode_hl, mode_str)
+            if info_content ~= '' then
+              left_segment = left_segment .. string.format('%%#%sToDevinfo#%s%%#MiniStatuslineDevinfo# %s ', mode_hl, sep_right, info_content)
+              left_segment = left_segment .. string.format('%%#MiniStatuslineDevinfoExt#%s', sep_right)
+            else
+              left_segment = left_segment .. string.format('%%#%sExt#%s', mode_hl, sep_right)
+            end
+          end
+
+          local right_segment = ''
+          if location and location ~= '' then
+            if fileinfo ~= '' then
+              right_segment = string.format('%%#MiniStatuslineDevinfoExt#%s%%#MiniStatuslineDevinfo# %s ', sep_left, fileinfo)
+              right_segment = right_segment .. string.format('%%#DevinfoTo%s#%s%%#%s# %s %%#%sExt#%s', mode_hl, sep_left, mode_hl, location, mode_hl, sep_right)
+            else
+              right_segment = string.format('%%#%sExt#%s%%#%s# %s %%#%sExt#%s', mode_hl, sep_left, mode_hl, location, mode_hl, sep_right)
+            end
+          end
+
+          return table.concat({
+            left_segment,
+            '  ',
+            '%#MiniStatuslineFilename#',
+            filename,
+            '%=',
+            search,
+            '  ',
+            right_segment,
+          }, '')
+        end,
+      },
+    }
 
     -- Run once and whenever the theme changes
     sync_hl()
